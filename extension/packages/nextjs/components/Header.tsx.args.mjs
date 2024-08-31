fff461233f0fee8a7ed5fa53598bd9fe35400f55
Eda Akturk <edaakturk@192.168.1.111>
2024-08-31 13:56:43 +0300

    Add header tab

---
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -0,0 +1,6 @@
+export const menuIconImports = `import { PhotoIcon } from "@heroicons/react/24/outline";`;
+export const menuObjects = `{
+  label: "NFT",
+  href: "/nft",
+  icon: <PhotoIcon className="h-4 w-4" />,
+}`;
